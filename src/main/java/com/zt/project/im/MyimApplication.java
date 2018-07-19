b610b2bdc3329b0e04bc62b2617d0f79823dbe06
bolatusad <563375002@qq.com>
2018-07-19 10:18:24 +0800

    test branch

--- src/main/java/com/zt/project/im/MyimApplication.java
+++ src/main/java/com/zt/project/im/MyimApplication.java
@@ -3,6 +3,9 @@ package com.zt.project.im;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * test develop branch
+ */
 @SpringBootApplication
 public class MyimApplication {
 
